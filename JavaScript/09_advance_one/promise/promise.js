96ad73cebfb520495618c62229480e0223d98eb3
shubhf18425 <shubh.aglf@gmail.com>
2025-05-03 01:24:59 +0530

    promises

--- JavaScript/09_advance_one/promise/promise.js
+++ JavaScript/09_advance_one/promise/promise.js
@@ -1,4 +1,4 @@
-// fetch('https://something').then().catch().finally()
+fetch('https://something').then().catch().finally()
 
 const promiseone = new Promise((resolve, reject) => {
     // do an async task 
@@ -74,4 +74,67 @@ promisefour
 })
 .finally(()=>{
     console.log('Promise four completed'); // This will always run
+})
+
+const promisefive = new Promise((resolve , reject)=>{
+    setTimeout(()=>{
+        let error = true;
+        if (error) {
+            reject('An error occurred in promisefive');
+        } else {
+            resolve({username: 'primse5' , password: '1234'});
+        }
+    } , 1000)
+})
+// async function consumepromisefive(){
+//     const response = await promisefive;
+//     console.log(response);
+
+//     console.log(`Username from promisefive is: ${response.username}`);
+//     console.log(`Password from promisefive is: ${response.password}`);
+// }
+// consumepromisefive()    
+async function consumepromisefive(){
+    try{
+        const response = await promisefive;
+    console.log(response);
+
+    console.log(`Username from promisefive is: ${response.username}`);
+    console.log(`Password from promisefive is: ${response.password}`);
+    } catch(error){
+        console.log('Error:', error); // Handle the error appropriately
+    }
+    
+}
+consumepromisefive()    
+
+async function fetchData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = response.json();
+    console.log(data);
+}
+fetchData()
+
+async function fetchData() {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log('Error:', error); // Handle the error appropriately
+    } 
+   
+}
+fetchData()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((res) =>{
+return res.json();
+})
+.then((data) => {
+console.log(data);
+})
+.catch((err) =>{
+console.log(err);
 })
